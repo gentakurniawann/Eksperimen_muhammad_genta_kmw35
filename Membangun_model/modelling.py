d623df05e035021a39656ee0c3f730a7b79d5fb9
gentakurniawann <mgentakurniawan6@gmail.com>
2026-07-24 21:34:47 +0700

    feat: add Membangun_model with modelling scripts and screenshots

---
+++ Membangun_model/modelling.py
@@ -0,0 +1,26 @@
+import os
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+import mlflow
+
+# Requirement: Localhost MLFlow tracking for basic
+os.environ['MLFLOW_TRACKING_URI'] = "http://localhost:5000"
+
+def main():
+    try:
+        df = pd.read_csv('../Eksperimen_SML_muhammad_genta_kmw35/iris_preprocessing/iris_processed.csv')
+    except FileNotFoundError:
+        print("Data not found. Please run automate_muhammad_genta_kmw35.py first.")
+        return
+        
+    X = df.drop(columns=['target'])
+    y = df['target']
+    
+    mlflow.sklearn.autolog()
+    
+    with mlflow.start_run():
+        model = RandomForestClassifier(n_estimators=10, random_state=42)
+        model.fit(X, y)
+
+if __name__ == "__main__":
+    main()
